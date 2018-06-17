4dc5199d2eebef2d7906dfbcd05dc558b9713733
Javierimperial <javiersanchezugalde@gmail.com>
2018-06-16 18:43:47 -0600

    Time is up

---
+++ TestCecropia/app/scripts/main.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+    
+
+    var $divs = $('div.app');
+
+	$('#alphBntAZ').on('click', function () {
+	    var alphabeticallyOrderedDivs = $divs.sort(function (a, b) {
+	        return $(a).find('h5').text() > $(b).find('h5').text();
+	    });
+	    $('#app-board').html(alphabeticallyOrderedDivs);
+	});
+
+	$('#alphBntZA').on('click', function () {
+	    var alphabeticallyOrderedDivs = $divs.sort(function (a, b) {
+	        return $(b).find('h5').text() > $(a).find('h5').text();
+	    });
+	    $('#app-board').html(alphabeticallyOrderedDivs);
+	});
+});
